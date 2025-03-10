a87653a52b047a5f000abb41f3da6b42383210e7
Jedoth <emivirdisla@gmail.com>
2025-03-10 16:25:37 -0400

    .

--- src/commands/management/moderation/mod.js
+++ src/commands/management/moderation/mod.js
@@ -1,25 +1,16 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, MessageFlags } = require('discord.js');
 const kick = require('./subcommands/kick');
-const ban = require('./subcommands/ban');
-const unban = require('./subcommands/unban');
-const mute = require('./subcommands/mute');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('mod')
     .setDescription('Comandos de moderación')
     .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
-    .addSubcommand(kick.data)
-    .addSubcommand(ban.data)
-    .addSubcommand(unban.data)
-    .addSubcommand(mute.data),
+    .addSubcommand(kick.data),
 
   async execute(interaction) {
     const subcommands = {
-      'kick': kick.execute,
-      'ban': ban.execute,
-      'unban': unban.execute,
-      'mute': mute.execute
+      'kick': kick.execute
     };
 
     const subcommand = interaction.options.getSubcommand();
@@ -28,7 +19,7 @@ module.exports = {
     if (commandFunc) {
       await commandFunc(interaction);
     } else {
-      await interaction.reply({ content: 'Subcomando no reconocido.', ephemeral: true });
+      await interaction.reply({ content: 'Subcomando no reconocido.', flags: MessageFlags.Ephemeral });
     }
   },
 };
