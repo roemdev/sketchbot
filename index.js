6a6cb3c2efcd330107bb97f76aed539fce01cc11
emivirdisla <emivirdisla@gmail.com>
2025-09-30 18:20:06 +0000

    .

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessageReactions,
