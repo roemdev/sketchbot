1aff2c4a24b0b48553a1aba1b82234aedd04e1e6
Jedoth <emivirdisla@gmail.com>
2025-12-11 17:25:08 -0400

    .

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
-const fs = require('fs');
-const path = require('path');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
 client.commands = new Collection();
 
 // Cargar comandos
-const commandsPath = path.join(__dirname, 'commands');
-for (const folder of fs.readdirSync(commandsPath)) {
-  const folderPath = path.join(commandsPath, folder);
-  for (const file of fs.readdirSync(folderPath).filter(f => f.endsWith('.js'))) {
-    const command = require(path.join(folderPath, file));
-    if (command.data) client.commands.set(command.data.name, command);
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
   }
 }
 
